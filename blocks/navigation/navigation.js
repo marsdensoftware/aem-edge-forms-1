8f017712164dc1adc68bbb4cb3c4036e41a66ff5
Seno <seno.is.dev@gmail.com>
2025-04-22 14:13:59 +1200

    feat(block): user indicator

--- blocks/navigation/navigation.js
+++
@@ -1,7 +0,0 @@
-export default function decorate(block) {
-  const [navigationWrapper] = block.children;
-
-  const nav = document.createElement('nav');
-  nav.textContent = navigationWrapper.textContent.trim();
-  navigationWrapper.replaceChildren(nav);
-}
