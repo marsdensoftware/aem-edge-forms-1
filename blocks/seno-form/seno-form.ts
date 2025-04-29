c9fbf0076ce7037982db16b63cd0e7dd10eedec2
Seno <seno.is.dev@gmail.com>
2025-04-29 17:30:23 +1200

    feat: storing URL parameters

--- blocks/seno-form/seno-form.ts
+++ blocks/seno-form/seno-form.ts
@@ -1,10 +1,21 @@
+const getLocalTheme = (): string | null => {
+    const theme = window.localStorage.getItem('theme')
+    if (!theme) return null
+    return theme
+}
+
 function decorate(block: Element) {
     const innerContainer = document.createElement('div')
     innerContainer.classList.add('seno-form__inner')
 
+    const title = document.createElement('h2')
+    title.className = 'seno-form__title'
+    title.innerText = 'Local storage'
+
     const pickedColour = document.createElement('p')
-    const colour =
-        window.localStorage.getItem('theme') ?? 'no colour yet selected'
+    const colour = window.localStorage.getItem('theme')
+        ? `Your selected theme is ${window.localStorage.getItem('theme')}`
+        : 'Please select a colour'
     pickedColour.innerText = colour
 
     const button1 = document.createElement('button')
@@ -16,27 +27,94 @@ function decorate(block: Element) {
     button2.innerText = 'Blue'
 
     button1.addEventListener('click', () => {
-        window.localStorage.setItem('theme', 'Red')
+        window.localStorage.setItem('theme', 'red')
         window.location.reload()
     })
 
     button2.addEventListener('click', () => {
-        window.localStorage.setItem('theme', 'Blue')
+        window.localStorage.setItem('theme', 'blue')
         window.location.reload()
     })
 
     const deleteBtn = document.createElement('button')
     deleteBtn.innerText = 'Delete'
     deleteBtn.addEventListener('click', () => {
-        const theme = window.localStorage.getItem('theme')
-
-        if (!theme) return
+        if (!getLocalTheme()) return
 
         window.localStorage.removeItem('theme')
         window.location.reload()
     })
 
-    innerContainer.append(pickedColour, button1, button2, deleteBtn)
+    /*
+        Persist search filter
+    */
+    const title2 = document.createElement('h2')
+    title2.className = 'seno-form__title2'
+    title2.innerText = 'Persist search filter'
+
+    // FORM
+    const radioOpt1 = document.createElement('input')
+    const radioOpt1Label = document.createElement('label')
+    const radioOpt2 = document.createElement('input')
+    const radioOpt2Label = document.createElement('label')
+    radioOpt1Label.innerText = 'Red'
+    radioOpt2Label.innerText = 'Blue'
+    radioOpt1Label.setAttribute('for', 'red')
+    radioOpt2Label.setAttribute('for', 'blue')
+    radioOpt1.setAttribute('id', 'red')
+    radioOpt1.setAttribute('name', 'theme')
+    radioOpt1.setAttribute('type', 'radio')
+    radioOpt1.setAttribute('value', 'red')
+    radioOpt2.setAttribute('id', 'blue')
+    radioOpt2.setAttribute('name', 'theme')
+    radioOpt2.setAttribute('type', 'radio')
+    radioOpt2.setAttribute('value', 'blue')
+    radioOpt1.className = 'seno-form__radio1'
+    radioOpt2.className = 'seno-form__radio2'
+    radioOpt1Label.className = 'seno-form__label1'
+    radioOpt2Label.className = 'seno-form__label2'
+
+    const form = document.createElement('form')
+    form.append(radioOpt1Label, radioOpt1, radioOpt2Label, radioOpt2)
+
+    // Get URL
+    const url = new URL(window.location.href)
+
+    // if (window.onload) {
+    // document.onload = () => {
+    //     if (getLocalTheme()) {
+    //         // Set search param
+    //         url.searchParams.set('theme', `${getLocalTheme()}`)
+    //         // Added in history
+    //         window.history.pushState({}, '', url.href)
+    //     }
+    // }
+    // }
+
+    form.addEventListener('change', () => {
+        // Get value from form
+        const radio = form.elements
+        const { value } = radio.namedItem('theme') as RadioNodeList
+
+        // Set search param
+        url.searchParams.set('theme', `${value}`)
+        // Added in history
+        window.history.pushState({}, '', url.href)
+        // Set in localstorage
+        window.localStorage.setItem('theme', value)
+        console.log(getLocalTheme())
+    })
+
+    innerContainer.append(
+        title,
+        pickedColour,
+        button1,
+        button2,
+        deleteBtn,
+        title2,
+        form,
+    )
+
     block.append(innerContainer)
 }
 
